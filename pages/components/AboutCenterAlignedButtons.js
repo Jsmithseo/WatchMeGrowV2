11757af8d683d609d2b9199b2078848d4d0605c7
SmittyHamm <smittyhamm@Chloes-MacBook-Air.local>
2023-08-29 19:19:55 -0700

    added space for donation tile

--- pages/components/AboutCenterAlignedButtons.js
+++ pages/components/AboutCenterAlignedButtons.js
@@ -4,28 +4,28 @@ import Link from 'next/link';
 
 const mainStyle = {
   marginTop: '10px',
-  paddingTop: '100px',
-  paddingBottom: '100px',
+  paddingTop: '50px',
+  paddingBottom: '50px',
 };
 
 const firstButton = {
-  backgroundColor: ' #FBA9B1',
+  backgroundColor: ' #DFFFD6',
 };
 
 const secondButton = {
-  backgroundColor: '#F27C8C',
+  backgroundColor: '#BFEAB7',
 };
 
 const thirdButton = {
-  backgroundColor: '#E85065',
+  backgroundColor: '#9FD598',
 };
 
 const fourthButton = {
-  backgroundColor: '#D9243F',
+  backgroundColor: '#7FC079',
 };
 
 const fifthButton = {
-  backgroundColor: '#C11C27',
+  backgroundColor: '#278754',
 };
 
 function AboutCenterAlignedButtons() {
@@ -35,11 +35,12 @@ function AboutCenterAlignedButtons() {
         <div className="text-center">
           <h2>Your gift today can help change a child’s future.</h2>
         </div>
+        <br></br>
         <Row className="justify-content-center">
           <Col md="auto" sm="2" xs="2">
             
             <Button style={firstButton} className="mx-2">
-            <Link href="https://buy.stripe.com/9AQcNj3KD0pY0Bq9AA" style={{ textDecoration: 'none' }}>
+            <Link href="https://buy.stripe.com/9AQcNj3KD0pY0Bq9AA" style={{ textDecoration: 'none' ,  color: 'black'}}>
               $25
               </Link>
             </Button>
@@ -49,7 +50,7 @@ function AboutCenterAlignedButtons() {
           <Col md="auto" sm="2" xs="2">
             
             <Button style={secondButton} className="mx-2">
-            <Link href="https://buy.stripe.com/6oEdRn0yra0y3NC3cd" style={{ textDecoration: 'none' }}>
+            <Link  href="https://buy.stripe.com/6oEdRn0yra0y3NC3cd" style={{ textDecoration: 'none', color: 'black' }}>
               $50
               </Link>
             </Button>
@@ -59,7 +60,7 @@ function AboutCenterAlignedButtons() {
           <Col md="auto" sm="2" xs="2">
             
               <Button style={thirdButton} className="mx-2">
-              <Link href="https://buy.stripe.com/9AQ5kReph3CafwkdQS" style={{ textDecoration: 'none' }}>
+              <Link href="https://buy.stripe.com/9AQ5kReph3CafwkdQS" style={{ textDecoration: 'none',  color: 'black' }}>
                 $100
                 </Link>
               </Button>
@@ -69,7 +70,7 @@ function AboutCenterAlignedButtons() {
           <Col md="auto" sm="2" xs="2">
             
               <Button style={fourthButton} className="mx-2">
-              <Link href="https://buy.stripe.com/4gwaFb3KDc8Gdoc6or" style={{ textDecoration: 'none' }}>
+              <Link href="https://buy.stripe.com/4gwaFb3KDc8Gdoc6or" style={{ textDecoration: 'none',  color: 'black' }}>
                 $250
                 </Link>
               </Button>
@@ -79,7 +80,7 @@ function AboutCenterAlignedButtons() {
           <Col md="auto" sm="2" xs="2">
             
               <Button style={fifthButton} className="mx-2">
-              <Link href="https://buy.stripe.com/00g3cJgxp3Ca1Fu3cg" style={{ textDecoration: 'none' }}>
+              <Link href="https://buy.stripe.com/00g3cJgxp3Ca1Fu3cg" style={{ textDecoration: 'none',  color: 'black' }}>
                 $500
                 </Link>
               </Button>
