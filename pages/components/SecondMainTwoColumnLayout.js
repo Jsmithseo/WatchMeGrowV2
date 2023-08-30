8e7a2b28ec0d3ebfb7946b7940a70af2c18ff277
SmittyHamm <smittyhamm@Chloes-MacBook-Air.local>
2023-08-29 19:02:55 -0700

    adjust color to links for donation buttons

--- pages/components/SecondMainTwoColumnLayout.js
+++ pages/components/SecondMainTwoColumnLayout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container, Row, Col, Button } from 'reactstrap';
-
+import AboutCenterAlignedButtons from './AboutCenterAlignedButtons';
 
 const buttonStyle = {
   align: 'center',
@@ -64,6 +64,7 @@ const SecondMainTwoColumnLayout = () => {
             </p>
           </Col>
         </Row>
+        <AboutCenterAlignedButtons/>
       </Container>
     </div>
   );
