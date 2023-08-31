a88ffae823d1c07574b500576ee73ae87146ce40
SmittyHamm <smittyhamm@Chloes-MacBook-Air.local>
2023-08-31 15:42:05 -0700

    change back ground color on donatation page

--- pages/donate/index.js
+++ pages/donate/index.js
@@ -2,48 +2,40 @@ import React from 'react';
 import Header from '../components/Header';
 import 'bootstrap/dist/css/bootstrap.css';
 import AboutCenterAlignedButtons from '../components/AboutCenterAlignedButtons';
-import NewsletterForm from '../components/NewsletterForm';
 import Footer from '../components/MainFooter';
 import DonateBanner from '../components/DonateBanner';
 import DonatationTile from '../components/DonationTiles';
 import { Container, Row, Col, Button } from 'reactstrap';
 
-
 export default function Donate() {
   return (
     <div className="mainStyle">
-  <style jsx>
+      <style jsx>
+        {`
+          .mainStyle {
+            overflow-x: hidden;
+            background-color: #EFEFEF;
+          }
 
-    {`
+          .copy {
+            text-align: center;
+            font-family: avenir;
+            padding-top: 50px;
+            font-size: 22px;
+            margin-right: 100px;
+            margin-left: 100px;
+          }
 
-      .mainStyle {
-        overflow-x: hidden;
-      }
-
-      .copy {
-        text-align: center;
-        font-family: avenir;
-        padding-top: 50px;
-        font-size: 22px;
-        margin-right: 100px;
-        margin-left: 100px;
-  
-      }
-
-      @media (max-width: 600px) {
-           
-        .copy {
-          margin-right: 5px;
-          margin-left: 5px;
-          padding-top: 20px;
-          margin-bottom: -40px;
-
-        }
-
-      }
-      
-    `}
-  </style>
+          @media (max-width: 600px) {
+            .copy {
+              margin-right: 5px;
+              margin-left: 5px;
+              padding-top: 20px;
+              margin-bottom: -40px;
+            }
+          }
+        `}
+      </style>
       <Header />
       <DonateBanner />
       <Col>
@@ -53,7 +45,7 @@ export default function Donate() {
           also fosters a brighter future for those we serve.
         </div>
       </Col>
-      <AboutCenterAlignedButtons/>
+      <AboutCenterAlignedButtons />
       <DonatationTile />
       <Footer />
     </div>
